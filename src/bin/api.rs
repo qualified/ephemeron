fc41229bd46a31e175b8992313ab91e3bd418bfd
kazk <kazk.dev@gmail.com>
2021-06-17 23:07:08 -0700

    Change api address to 0.0.0.0

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -10,7 +10,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "ephemeron=trace".to_owned());
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "info,ephemeron=debug".to_owned());
     tracing_subscriber::fmt()
         .with_env_filter(filter)
         .with_span_events(FmtSpan::CLOSE)
@@ -22,6 +22,6 @@ async fn main() -> Result<()> {
         .allow_headers(&[header::AUTHORIZATION, header::CONTENT_TYPE])
         .allow_methods(&[Method::POST, Method::GET, Method::DELETE, Method::OPTIONS]);
     let api = ephemeron::api::new(client).with(cors);
-    warp::serve(api).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(api).run(([0, 0, 0, 0], 3030)).await;
     Ok(())
 }
